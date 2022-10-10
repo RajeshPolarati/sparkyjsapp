1c3b61afd4983f9017a64cd9f7c918e638f7bdfd
RajeshPolarati <87992930+RajeshPolarati@users.noreply.github.com>
2022-10-10 23:09:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { PublicKey } from 'snarkyjs';
+//import { PrivateKey } from 'snarkyjs';
+// import {NewContract} from 'examplesnarky0'
+
+function App() {
+  useEffect(() => {
+    (async () => {
+      const { NewContract } = await import("examplesnarky0");
+      await NewContract.compile()
+      let address = 'B62qn8TChAWtuUFBcPQfb6UyTuZHTDbwySdiChdMaGSU3xdqFiivaXM'
+      const contract = new NewContract(PublicKey.fromBase58(address))
+      let val = contract.num.get()
+      console.log(val);
+      console.log(val.toBase58());
+      // console.log(contract);
+    })();
+  }, []);
+  return (
+    <div className="App">
+      React App
+    </div>
+  );
+}
+
+export default App;
